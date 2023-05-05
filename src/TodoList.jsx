5de9a480e5e9dfc9674d8b5ab8f1787951d4745c
Carl Elgström <carlelgstrom@Carls-MacBook-Pro.local>
2023-05-05 13:37:38 -0400

    Removed title in html, and updated lable

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,7 +3,7 @@ import { TodoItem } from "./TodoItem"
 export function TodoList({todos, toggleTodo, deleteTodo}){
     return (
 <ul className="list">
-{todos.length === 0 && "No todos, yay!"}
+{todos.length === 0 && "No todos, woho!"}
 {todos.map(todo => {
   return (
     <TodoItem {...todo} key={todo.key} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
