b69af882139ac4fa11d36b2b7baa7c74dfc8ea02
Carl Elgström <carlelgstrom@Carls-MacBook-Pro.local>
2023-05-05 13:34:32 -0400

    All files for react todo app

---
+++ src/TodoList.jsx
@@ -0,0 +1,14 @@
+import { TodoItem } from "./TodoItem"
+
+export function TodoList({todos, toggleTodo, deleteTodo}){
+    return (
+<ul className="list">
+{todos.length === 0 && "No todos, yay!"}
+{todos.map(todo => {
+  return (
+    <TodoItem {...todo} key={todo.key} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+)
+})}
+</ul>
+    )
+}
